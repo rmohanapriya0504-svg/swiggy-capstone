9017deec7f54de6ec884fdbc70235af8392a96e6
rmohanapriya0504-svg <rmohanapriya.0504@gmail.com>
2026-08-11 14:27:05 +0530

    Update 01_foundations.sql

--- sql/01_foundations.sql
+++ sql/01_foundations.sql
@@ -1,1 +1,46 @@
+-- WHERE
+SELECT *
+FROM restaurants
+WHERE city = 'Mumbai';
 
+
+-- DISTINCT
+SELECT DISTINCT cuisine
+FROM restaurants;
+
+
+-- ORDER BY + LIMIT
+SELECT *
+FROM orders
+ORDER BY amount_inr DESC
+LIMIT 5;
+
+
+-- LIKE
+SELECT *
+FROM restaurants
+WHERE name LIKE '%Spice%';
+
+
+-- IN
+SELECT *
+FROM customers
+WHERE city IN ('Mumbai', 'Delhi');
+
+
+-- BETWEEN
+SELECT *
+FROM orders
+WHERE amount_inr BETWEEN 500 AND 1500;
+
+
+-- NOT BETWEEN
+SELECT *
+FROM orders
+WHERE amount_inr NOT BETWEEN 500 AND 1500;
+
+
+-- IS NULL
+SELECT *
+FROM orders
+WHERE rating IS NULL;
